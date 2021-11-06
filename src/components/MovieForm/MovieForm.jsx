d446a1760a5a230bcd5b88a599596192dd42cf55
Austin Kammerer <austin4191@gmail.com>
2021-11-06 17:19:39 -0500

    local state for holding form inputs

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -10,13 +10,36 @@ export default function MovieForm() {
   // get the genre list
   const genres = useSelector((store) => store.genres);
 
+  // local state to store inputs
+  const [newMovie, setNewMovie] = useState({
+    title: "",
+    poster: "",
+    description: "",
+    genre: "",
+  });
+
+  const handleChange = (e) => {
+    // grab the name and value of the input being changed
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
+    // change the newMovie state according to which input is being changed
+    // if the current property being set is genre, put the value in an array
+    setNewMovie({ ...newMovie, [name]: name === "genre" ? [value] : value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newMovie);
+  };
+
   return (
     <main>
-      <form>
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Poster URL" />
-        <input type="text" placeholder="Description" />
-        <select name={genres}>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
+        <input name="title" type="text" placeholder="Title" />
+        <input name="poster" type="text" placeholder="Poster URL" />
+        <input name="description" type="text" placeholder="Description" />
+        <select name="genre">
           <option>--Select a Genre--</option>
           {genres.map((genre, i) => (
             <option key={i} value={genre.name}>
@@ -24,6 +47,7 @@ export default function MovieForm() {
             </option>
           ))}
         </select>
+        <button type="submit">Add</button>
       </form>
     </main>
   );
