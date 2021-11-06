d96ac28458c5fd706d11f22d491bfc9cf4aa8153
Austin Kammerer <austin4191@gmail.com>
2021-11-06 16:59:00 -0500

    new movie form wireframe

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,13 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function MovieForm() {
+  const dispatch = useDispatch();
+  // save the genres in the store on page load for the dropdown
+  useEffect(() => {
+    dispatch({ type: "FETCH_GENRES" });
+  }, []);
+  // get the genre list
+  const genres = useSelector((store) => store.genres);
+
   return (
     <main>
       <form>
         <input type="text" placeholder="Title" />
         <input type="text" placeholder="Poster URL" />
         <input type="text" placeholder="Description" />
+        <select name={genres}>
+          <option>--Select a Genre--</option>
+          {genres.map((genre, i) => (
+            <option key={i} value={genre}></option>
+          ))}
+        </select>
       </form>
     </main>
   );
