8823131e3fcafb8ccd22f15b8ecbdd9967e13f3e
Austin Kammerer <austin4191@gmail.com>
2021-12-06 21:18:20 -0600

    fixed bug

--- src/components/AdminLogin/AdminLogin.jsx
+++ src/components/AdminLogin/AdminLogin.jsx
@@ -1,116 +1,116 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-
 import Box from "@mui/material/Box";
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
 
 export default function AdminLogin() {
-    const history = useHistory();
+  const history = useHistory();
 
-    // for the form dialogue
-    const [open, setOpen] = useState(false);
-    const [retryOpen, setRetryOpen] = useState(false);
-    const [credentials, setCredentials] = useState({ username: "", password: "" })
+  // for the form dialogue
+  const [open, setOpen] = useState(false);
+  const [retryOpen, setRetryOpen] = useState(false);
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
-    // handle open/close of main login dialogue
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  // handle open/close of main login dialogue
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    // handle open/close of failed login message dialogue
-    const handleRetryClickOpen = () => {
-        setRetryOpen(true);
-    };
+  // handle open/close of failed login message dialogue
+  const handleRetryClickOpen = () => {
+    setRetryOpen(true);
+  };
 
-    const handleRetryClose = () => {
-        setRetryOpen(false);
-        setCredentials({ username: "", password: "" })
-    };
+  const handleRetryClose = () => {
+    setRetryOpen(false);
+    setCredentials({ username: "", password: "" });
+  };
 
-    // handle input changes for logging in
-    const handleChange = (e) => {
-        // grab the name and value of the input being changed
-        const { name, value } = e.target;
-        console.log(name);
-        console.log(value);
-        // change the credentials state according to which input is being changed
-        setCredentials({ ...credentials, [name]: value });
-    };
+  // handle input changes for logging in
+  const handleChange = (e) => {
+    // grab the name and value of the input being changed
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
+    // change the credentials state according to which input is being changed
+    setCredentials({ ...credentials, [name]: value });
+  };
 
-    // login button will either move to admin page or open fail dialogue
-    const handleLogin = () => {
-        if (credentials.username === 'camera' && credentials.password == 'action') {
-            history.push('/admin')
-        } else {
-            handleRetryClickOpen();
-        }
+  // login button will either move to admin page or open fail dialogue
+  const handleLogin = () => {
+    if (credentials.username === "camera" && credentials.password == "action") {
+      history.push("/admin");
+      handleClose();
+    } else {
+      handleRetryClickOpen();
     }
-    return (
-        <>
-            <Button variant="contained" onClick={handleClickOpen} >
-                Admin Login
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle id="admin-login">Admin Login</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Please enter you username and password.
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        name="username"
-                        value={credentials.username}
-                        margin="normal"
-                        label="Username"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="password"
-                        value={credentials.password}
-                        margin="normal"
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        variant="outlined"
-                        onChange={handleChange}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleLogin}>Login</Button>
-                </DialogActions>
-            </Dialog>
-            {/* Dialog for login fail */}
-            <Dialog
-                open={retryOpen}
-                onClose={handleRetryClose}
-            >
-                <DialogTitle id="login-retry">
-                    {"Invalid Username/Password Combination"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Please try again!
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleRetryClose}>Retry</Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    )
+  };
+  return (
+    <>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Admin Login
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle id="admin-login">Admin Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter you username and password.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            name="username"
+            value={credentials.username}
+            margin="normal"
+            label="Username"
+            type="text"
+            fullWidth
+            variant="outlined"
+            onChange={handleChange}
+          />
+          <TextField
+            name="password"
+            value={credentials.password}
+            margin="normal"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="outlined"
+            onChange={handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleLogin}>Login</Button>
+        </DialogActions>
+      </Dialog>
+      {/* Dialog for login fail */}
+      <Dialog open={retryOpen} onClose={handleRetryClose}>
+        <DialogTitle id="login-retry">
+          {"Invalid Username/Password Combination"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Please try again!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleRetryClose}>Retry</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,50 +15,50 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // add a dark theme
 const darkTheme = createTheme({
-    palette: {
-        mode: "dark",
-    },
+  palette: {
+    mode: "dark",
+  },
 });
 
 function App() {
-    // grab the current page's path for header conditional rendering
-    const path = useSelector((store) => store.path);
+  // grab the current page's path for header conditional rendering
+  const path = useSelector((store) => store.path);
 
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <Container
-                className="App"
-                maxWidth="xl"
-                sx={{ textAlign: "center", px: 0 }}
-            >
-                <Router>
-                    {/* <Typography variant="h2" component="h1" mb={2} color="white">
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container
+        className="App"
+        maxWidth="xl"
+        sx={{ textAlign: "center", px: 0 }}
+      >
+        <Router>
+          {/* <Typography variant="h2" component="h1" mb={2} color="white">
             {path === "/admin" ? "Welcome, Admin" : "My Movies"}
           </Typography> */}
-                    <Header />
-                    <Route path="/" exact>
-                        <MovieList />
-                    </Route>
-                    {/* Details page */}
-                    <Route path={"/details/:id"}>
-                        <MovieDetails />
-                    </Route>
-                    {/* Add Movie page */}
-                    <Route path={"/new"}>
-                        <MovieForm />
-                    </Route>
-                    {/* Edit Movie page */}
-                    <Route path={"/edit/:id"}>
-                        <EditMovie />
-                    </Route>
-                    {/* Admin page */}
-                    <Route path={"/admin"}>
-                        <AdminView />
-                    </Route>
-                </Router>
-            </Container>
-        </ThemeProvider>
-    );
+          <Header />
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+          {/* Details page */}
+          <Route path={"/details/:id"}>
+            <MovieDetails />
+          </Route>
+          {/* Add Movie page */}
+          <Route path={"/new"}>
+            <MovieForm />
+          </Route>
+          {/* Edit Movie page */}
+          <Route path={"/edit/:id"}>
+            <EditMovie />
+          </Route>
+          {/* Admin page */}
+          <Route path={"/admin"}>
+            <AdminView />
+          </Route>
+        </Router>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
